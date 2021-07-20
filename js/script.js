7afccb5ff53c40f737a549cea33c88d012af486e
Cormac Smith <cmac@intruder.local>
2021-07-20 17:19:07 +0100

    changed skill colours

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 	});
 
 	$('.owl-carousel').owlCarousel({
-	    loop:true,
+	    loop:false,
 	    items: 4,
 	    responsive:{
 	    	// 0 to 480 pixels use 1 item
@@ -29,7 +29,6 @@ $(document).ready(function(){
 	        768:{
 	            items:4
 	        }
-
 	    }
 	});
 
@@ -51,9 +50,45 @@ $(document).ready(function(){
             	}
     		});
 
-    		 $('.test').easyPieChart({
+    		 $('.item1').easyPieChart({
 	            easing: 'easeInOut',
 	            barColor: 	'#e74c3c',
+	            trackColor: false,
+	            scaleColor: false,
+	            lineWidth: 4,
+	            size: 152,
+	            onStep: function(from, to, percent) {
+	            	$(this.el).find('.percent').text(Math.round(percent));
+            	}
+    		});
+
+    		$('.item2').easyPieChart({
+	            easing: 'easeInOut',
+	            barColor: 	'#238636',
+	            trackColor: false,
+	            scaleColor: false,
+	            lineWidth: 4,
+	            size: 152,
+	            onStep: function(from, to, percent) {
+	            	$(this.el).find('.percent').text(Math.round(percent));
+            	}
+    		});
+
+    		$('.item3').easyPieChart({
+	            easing: 'easeInOut',
+	            barColor: 	'#f2c811',
+	            trackColor: false,
+	            scaleColor: false,
+	            lineWidth: 4,
+	            size: 152,
+	            onStep: function(from, to, percent) {
+	            	$(this.el).find('.percent').text(Math.round(percent));
+            	}
+    		});
+
+    		$('.item4').easyPieChart({
+	            easing: 'easeInOut',
+	            barColor: 	'#107c41',
 	            trackColor: false,
 	            scaleColor: false,
 	            lineWidth: 4,
